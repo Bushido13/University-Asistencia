1ece2ca59d26591682e3698336c07287a820226a
Bushido13 <eduardo.compan@hotmail.com>
2024-10-20 12:33:06 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'University',
+  webDir: 'www'
+};
+
+export default config;
