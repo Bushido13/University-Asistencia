809b881d517e03afc3b0af27bbe1ca8f96cd5e6b
Bushido13 <eduardo.compan@hotmail.com>
2024-11-22 15:19:00 -0300

    App-Final

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'cl.duoc.UniversityAsistencia',
-  appName: 'University Asistencia',
+  appName: 'University-Asistencia',
   webDir: 'www'
 };
 
